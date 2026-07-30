5df561d12ec8c88cfce66eaa4193fd8323cce10e
RavindraKS-nitte <ravindraks@gmail.com>
2026-07-30 14:56:26 +0530

    created ONNX model for STM32

--- STM32/Inference_OnBoard_IMU/X-CUBE-AI/App/network_data_params.h
+++ STM32/Inference_OnBoard_IMU/X-CUBE-AI/App/network_data_params.h
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file    network_data_params.h
   * @author  AST Embedded Analytics Research Platform
-  * @date    2026-07-30T12:42:33+0530
+  * @date    2026-07-30T14:55:30+0530
   * @brief   AI Tool Automatic Code Generator for Embedded NN computing
   ******************************************************************************
   * Copyright (c) 2026 STMicroelectronics.
